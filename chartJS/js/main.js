4d06c926fbb11809a72e046151845f4e78861ab8
paulwallin <steely_paul@hotmail.com>
2021-03-11 10:02:58 +0200

    chartJS exempel

---
+++ chartJS/js/main.js
@@ -0,0 +1,28 @@
+// Bar chart
+new Chart(document.getElementById('bar-chart'), {
+  type: 'pie',
+  data: {
+    labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America', 'Ny kontinent'],
+    datasets: [
+      {
+        label: 'Population (millions)',
+        backgroundColor: [
+          '#3e95cd',
+          '#8e5ea2',
+          '#3cba9f',
+          '#e8c3b9',
+          '#c45850',
+          '#993300'
+        ],
+        data: [2478, 5267, 734, 784, 433, 8000],
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: 'Predicted world population (millions) in 2050',
+    },
+  },
+})
